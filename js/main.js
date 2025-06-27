5477fada9f3c2a075b8a4baf65317b22a04b2c5a
kkyulee <kyuleenote5@gmail.com>
2025-06-27 16:24:39 +0900

    메인페이지 완성, footer완성

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-const bg=document.querySelector()
+const grid_bg = document.querySelectorAll(".con.content4 li");
+
+grid_bg.forEach((item, idx) => {
+  item.style.backgroundImage = `url('./images/grid0${idx + 1}_bg.jpg')`;
+  item.style.backgroundSize = "cover";
+  item.style.backgroundPosition = "center";
+  item.addEventListener('mouseenter',(e)=>{
+    
+  })
+});
