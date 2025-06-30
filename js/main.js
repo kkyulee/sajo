62c6ec473e1237f5b9cce472a03d92d665601f0c
kkyulee <kyuleenote5@gmail.com>
2025-07-01 03:11:41 +0900

    메인페이지 완성

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 import sajoData from "./data.js";
 const { telData, groupList } = sajoData;
+const prev = document.querySelector(".arrow .xi-angle-left-thin");
+const next = document.querySelector(".arrow .xi-angle-right-thin");
+const visualLis = document.querySelectorAll("#visual .scroll li");
 const grid_bg = document.querySelectorAll(".con.content4 li");
 const telBtn = document.querySelector(".tel-btn");
 const siteBtn = document.querySelector(".site-btn");
 const iClose = document.querySelector(".xi-close");
 const telPopup = document.querySelector(".tel-popup");
 
+let idx = 0;
+
+const showSlide = (i) => {
+  visualLis.forEach((li) => {
+    li.classList.remove("show");
+  });
+  visualLis[i].classList.add("show");
+};
+setInterval(() => {
+  idx = (idx + 1) % visualLis.length;
+  showSlide(idx);
+}, 5000);
+
+showSlide(idx);
+next.addEventListener("click", (e) => {});
 grid_bg.forEach((item, idx) => {
   item.style.backgroundImage = `url('./images/grid0${idx + 1}_bg.jpg')`;
   item.style.backgroundSize = "cover";
@@ -29,9 +47,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 iClose.addEventListener("click", (e) => {
-  telPopup.style.display = "none";
+  telPopup.classList.remove("active");
 });
-telBtn.addEventListener('click',(e)=>{
-    telPopup.style.display = "block";
-
-})
+telBtn.addEventListener("click", (e) => {
+  telPopup.classList.add("active");
+});
